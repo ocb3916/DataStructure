8f902e2fc4ef6abeb89728143a076b9d8cc280b3
ocb3916 <dhchanbin1004@gmail.com>
2023-10-28 17:14:43 +0900

    코드 가독성 수정

--- list/list.c
+++ list/list.c
@@ -3,13 +3,12 @@
 #define MAX_LIST_SIZE 100 // 리스트의 최대크기
 
 typedef int element; // 항목의 정의
-
 typedef struct {
     element array[MAX_LIST_SIZE]; // 배열 정의
     int size;                     // 현재 리스트에 저장된 항목들의 개수
 } ArrayListType;
 
-// 오류 처리 함수
+// 오류 처리 함수s
 void error(char *message) {
     fprintf(stderr, "%s\n", message);
     exit(1);
